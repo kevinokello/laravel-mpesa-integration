9bb8a57bf2e821c9ac8f061a9248fb7f0e14cc5f
kevinokello <okellokevin77@gmail.com>
2022-09-22 15:02:19 +0300

    setting up controllers and routes

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+Route::post('validation', [MPESAResponsesController::class, 'validation']);
+Route::post('confirmation', [MPESAResponsesController::class, 'confirmation']);
+Route::post('stkpush', [MPESAResponsesController::class, 'stkPush']);
+Route::post('b2ccallback', [MPESAResponsesController::class, 'b2cCallback']);
+Route::post('transaction-status/result_url', [MPESAResponsesController::class, 'transactionStatusResponse']);
+Route::post('reversal/result_url', [MPESAResponsesController::class, 'transactionReversal']);
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::post('get-token', [MPESAController::class, 'getAccessToken']);
+Route::post('register-urls', [MPESAController::class, 'registerURLS']);
+Route::post('simulate', [MPESAController::class, 'simulateTransaction']);
+Route::post('stkpush', [MPESAController::class, 'stkPush']);
+Route::post('simulateb2c', [MPESAController::class, 'b2cRequest']);
+Route::post('check-status', [MPESAController::class, 'transactionStatus']);
+Route::post('reversal', [MPESAController::class, 'reverseTransaction']);
+
+
+Route::get('stk', function () {
+    return view('stk');
+});
+
+Route::get('b2c', function () {
+    return view('b2c');
+});
+Route::get('transaction-status', function () {
+    return view('status');
+});
+Route::get('reverse', function () {
+    return view('reverse');
+});
